772889f279dac040cd355c446c1796c491f242ac
mhyeun <mhyeun131@gmail.com>
2021-01-08 01:33:48 -0500

    Add login, wall of fame, and wall of shame APIs

--- emojified_tweets_wall_of_fame/urls.py
+++ emojified_tweets_wall_of_fame/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main, name="main"),
+    path("wall-of-fame", views.wall_of_fame, name="wall_of_fame"),
+    path("wall-of-shame", views.wall_of_shame, name="wall_of_shame"),
     path("health", views.health, name="health"),
     path("authentication", views.authentication, name="authentication"),
 ]
